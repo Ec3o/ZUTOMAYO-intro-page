96f7d05c2386f43c69ef01015720215518fc63fa
Ec3o <2499302531@qq.com>
2024-11-17 10:52:02 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ZUTOMAYO-Intro-page
+
+简单糊的前端小项目，创新实践作业之一
---
+++ index.js
@@ -0,0 +1,53 @@
+ const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    autoplay: false,
+    theme: '#34495e',
+    loop: 'all',
+    order: 'list',
+    preload: 'auto',
+    volume: 0.7,
+    audio: [
+{
+    name: '低血ボルト',
+    artist: 'ずっと真夜中でいいのに',
+    url: 'assets/audio/ずっと真夜中でいいのに。 - 低血ボルト.mp3',
+    cover: 'assets/images/低血ボルト.jpg'
+},
+{
+    name: '秒針を噛む',
+    artist: 'ずっと真夜中でいいのに',
+    url: 'assets/audio/ずっと真夜中でいいのに。 - Ham.mp3',
+    cover: 'assets/images/秒針を噛む.jpg'
+},
+{
+    name: 'MILABO',
+    artist: 'ずっと真夜中でいいのに',
+    url: 'assets/audio/ずっと真夜中でいいのに。 - MILABO.mp3',
+    cover: 'assets/images/MILABO.jpg'
+},
+{
+    name: '嘘じゃない',
+    artist: 'ずっと真夜中でいいのに',
+    url: 'assets/audio/ずっと真夜中でいいのに。 - 嘘じゃない.mp3',
+    cover: 'assets/images/嘘じゃない.jpg'
+},
+{
+    name: '花一匁',
+    artist: 'ずっと真夜中でいいのに',
+    url: 'assets/audio/ずっと真夜中でいいのに。 - 花一匁.mp3',
+    cover: 'assets/images/花一匁.jpg'
+},
+{
+    name: '不法侵入',
+    artist: 'ずっと真夜中でいいのに',
+    url: 'assets/audio/ずっと真夜中でいいのに。 - 不法侵入.mp3',
+    cover: 'assets/images/不法侵入.jpg'
+},
+{
+    name: 'Ham',
+    artist: 'ずっと真夜中でいいのに',
+    url: 'assets/audio/ずっと真夜中でいいのに。 - Ham.mp3',
+    cover: 'assets/images/花一匁.jpg'
+}
+    ]
+});
